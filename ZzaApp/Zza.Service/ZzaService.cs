030b169bc49edf82b1c343add3f322ed68ea1327
albertmarsnik <albertmarsnik@gmail.com>
2019-10-21 22:57:39 +0200

    002_Implementing_Services/005_Demo_Service_Implementation _ Added [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall] to ZzaService. Which just cleans up memory after each call.

--- ZzaApp/Zza.Service/ZzaService.cs
+++ ZzaApp/Zza.Service/ZzaService.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Web;
 using Zza.Data;
 using Zza.Entities;
 
 namespace Zza.Service
 {
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class ZzaService : IZzaService, IDisposable
     {
         readonly ZzaDbContext _Context = new ZzaDbContext();
