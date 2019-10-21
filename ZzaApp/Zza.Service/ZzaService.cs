bcc44fff9e1af45101ca9f0f974f48769f090084
albertmarsnik <albertmarsnik@gmail.com>
2019-10-21 22:53:13 +0200

    002_Implementing_Services/005_Demo_Service_Implementation _ Added ZzaService.

---
+++ ZzaApp/Zza.Service/ZzaService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Zza.Data;
+using Zza.Entities;
+
+namespace Zza.Service
+{
+    public class ZzaService : IZzaService
+    {
+        readonly ZzaDbContext _Context = new ZzaDbContext();
+        public List<Customer> GetCustomers()
+        {
+            return _Context.Customers.ToList();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _Context.Products.ToList();
+        }
+
+        public void SubmitOrder(Order order)
+        {
+            _Context.Orders.Add(order);
+            order.OrderItems.ForEach(oi => _Context.OrderItems.Add(oi));
+            _Context.SaveChanges();
+        }
+    }
+}
