d2fdf1db21175e6905b9c3aa2302a1caa7cc137f
albertmarsnik <albertmarsnik@gmail.com>
2019-10-21 21:53:39 +0200

    002_Implementing_Services/004_Demo_Data_Contracts _ Added Data Contract tag to Product and Added Entities reference to Data Project.

--- ZzaApp/Zza.Entities/Product.cs
+++ ZzaApp/Zza.Entities/Product.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Zza.Entities
 {
+    [DataContract]
     public class Product
     {
         [DataMember]
--- ZzaApp/Zza.Service/IZzaService.cs
+++ ZzaApp/Zza.Service/IZzaService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using Zza.Entities;
 
 namespace Zza.Service
 {
