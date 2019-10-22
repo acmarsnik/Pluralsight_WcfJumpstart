dcece8c57c0c0cbd1df3bd32e8ff89f8ab0c4ec5
albertmarsnik <albertmarsnik@gmail.com>
2019-10-22 11:17:40 +0200

    003_Hosting_Services/007_Demo_Hosting_in_Console_App _ Added code to start the SelfHost Console App which listens on http://localhost:2112.

--- ZzaApp/Zza.SelfHost/Program.cs
+++ ZzaApp/Zza.SelfHost/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using Zza.Service;
 
 namespace Zza.SelfHost
 {
@@ -10,6 +13,20 @@ namespace Zza.SelfHost
     {
         static void Main(string[] args)
         {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(ZzaService));
+                host.Open();
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                // Log it
+                Debug.WriteLine(ex.Message);
+            }
+
         }
     }
 }
